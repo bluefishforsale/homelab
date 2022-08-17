059a5696c4dcd8b0da5150c5161a07e8b0e375f1
Terrac Skiens <3721396+bluefishforsale@users.noreply.github>
2022-08-16 18:15:28 -0700

    doc update

--- etcd_alias.sh
+++ etcd_alias.sh
@@ -9,14 +9,16 @@ export ETCDCTL_CACERT=/etc/etcd/ca.pem
 export ETCDCTL_CERT=/etc/etcd/kubernetes.pem
 export ETCDCTL_KEY=/etc/etcd/kubernetes-key.pem
 
-exit
-
 ETCDCTL_API=3 etcdctl member list --endpoints=https://127.0.0.1:2379 \
   --cacert=/etc/etcd/ca.pem \
   --cert=/etc/etcd/kubernetes.pem \
   --key=/etc/etcd/kubernetes-key.pem
 
-
+ETCDCTL_API=3 etcdctl endpoint health \
+  --endpoints=https://127.0.0.1:2379 \
+  --cacert=/etc/etcd/ca.pem \
+  --cert=/etc/etcd/kubernetes.pem \
+  --key=/etc/etcd/kubernetes-key.pem
 
 ETCDCTL_API=3 etcdctl del /registry/secrets/kube-node-lease/default-token-fgkrw \
   --endpoints=https://127.0.0.1:2379 \
@@ -25,11 +27,25 @@ ETCDCTL_API=3 etcdctl del /registry/secrets/kube-node-lease/default-token-fgkrw 
   --key=/etc/etcd/kubernetes-key.pem
 
 
-
 etcdctl get --prefix --keys-only /
 etcdctl member list --endpoints=https://127.0.0.1:2379
 etcdctl snapshot save ~/etcd.backup
 
+EPS=$(ETCDCTL_API=3 etcdctl member list \
+  --endpoints=https://127.0.0.1:2379 \
+  --cacert=/etc/etcd/ca.pem \
+  --cert=/etc/etcd/kubernetes.pem \
+  --key=/etc/etcd/kubernetes-key.pem \
+  | awk '{print $4}' \
+  | sed -e 's/2380/2379/g' \
+  | xargs | sed -e 's/\ //g' \
+  | sed -e 's/\ //g' -e 's/,$//g'
+)
+
+etcdctl endpoint health --endpoints="$EPS" \
+  --cacert=/etc/etcd/ca.pem \
+  --cert=/etc/etcd/kubernetes.pem \
+  --key=/etc/etcd/kubernetes-key.pem
 
 # delete all etcd
 ETCDCTL_API=3 \
