4b1414671c208b7a5a45964a210896fbd46a1ed2
Terrac Skiens <3721396+bluefishforsale@users.noreply.github.com>
2023-03-11 15:50:16 -0800

    common role shell tmux and stuff

---
+++ ansible-using-roles/roles/common/files/.oh-my-zsh/custom/aliases.sh
@@ -0,0 +1,10 @@
+alias log="sudo --no-pager journalctl -u $@"
+alias sys="sudo --no-pager systemctl $@"
+alias tf=terraform
+alias k=kubectl
+alias ktx=kubectx
+alias zshrc="source ${HOME}/.zshrc"
+alias ls=lsd
+
+
+export PATH="/usr/local/bin:$PATH"
