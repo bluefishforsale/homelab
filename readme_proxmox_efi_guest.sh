708ce8d0161ff157e0b2415b81261939e099f219
Terrac Skiens <terrac@cloudflare.com>
2023-08-09 22:33:30 -0700

    docs cleanup

--- readme_proxmox_efi_guest.sh
+++ readme_proxmox_efi_guest.sh
@@ -16,20 +16,11 @@ qm set 9999 --memory 4096
 qm set 9999 --agent enabled=1
 qm template 9999
 
-<EDIT CLOUDINIT USER PASSWORD>
 
 qm clone 9999 614
 qm set 614 --name kube614 --ipconfig0 ip=$(host kube614.home | awk '{print $NF}')/24,gw=192.168.1.1 --nameserver=192.168.1.2 --onboot 1
 qm set 614 --hostpci0=42:00,pcie=on,rombar=off,x-vga=off
 qm set 614 --cores 9
 qm set 614 --memory 57344  # 56G
+qm resize 614 scsi0 +18G  # 21.7G
 qm start 614
-
-rbd ls
-rbd  --image vm-9999-disk-0 info --pool ceph-lvm
-rbd map --pool ceph-lvm vm-9999-disk-0
-
-
-
-qm create 1001 --name tmp --net0 virtio,bridge=vmbr0
-qm importdisk 1001 debian-10-openstack-amd64.qcow2 ceph-lvm
