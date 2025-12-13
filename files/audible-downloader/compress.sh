9552937c955a260884e30a0c49df167ffff71ec0
Terrac Skiens <3721396+bluefishforsale@users.noreply.github.com>
2025-12-12 20:24:09 -0800

    audible downloader fixes

--- files/audible-downloader/compress.sh
+++ files/audible-downloader/compress.sh
@@ -5,6 +5,9 @@
 # Return the exit status of the last command in the pipeline that failed
 # set -o pipefail
 
+# Ensure pipx binaries are in PATH
+export PATH="/root/.local/bin:$PATH"
+
 BASEDIR='/data01/services/audible-downloader'
 TARGET='/data01/complete/audiobooks'
 
@@ -14,17 +17,13 @@ echo "Exporting audible library"
 audible library export
 
 find ./aax -type f -name '*.aax*' | sed 's|^\./||' |   while read file ; do
-    echo "Compressing ${TARGET}/aax/${file}"
+    echo "Compressing ${TARGET}/${file}"
     ${BASEDIR}/AAXtoMP3 \
-        -l 3 \
         --no-clobber \
         -A c424e208  \
         -t "${TARGET}/mp3" \
         -D '$artist - $title' \
         -e:mp3 \
         --chaptered \
-        --chapter-naming-scheme '$title - $(printf %0${#chaptercount}d $chapternum) $chapter' \
-        --use-audible-cli-data \
-        --audible-cli-library-file "${BASEDIR}/library.tsv" \
         "${TARGET}/${file}"
 done
